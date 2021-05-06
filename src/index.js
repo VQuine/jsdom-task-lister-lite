06f484ed19307f5d78d69296d9dafcb307e987fe
VQuine <01010110.v@gmail.com>
2021-05-06 06:50:56 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+
+
+//SET UP EVENT LISTENER - when the page is loaded
+//GRAB ELEMENTS/NODES - to refer to later
+//HANDLE SUBMIT BUTTON - grab the user action
+//OVERRIDE THE DEFAULT SUBMIT - append submission to DOM
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  //NODES (i think?)
+  const userInput = document.getElementById('new-task-description');
+  const userTasks = document.getElementById("tasks");
+  const userSubmit = document.querySelector('form');
+  
+  //INTERCEPT EVENT ON FORM ELEMENT
+  userSubmit.addEventListener("submit", event => {
+    event.preventDefault()
+
+    //APPEND DATA TO DOM
+    userTasks.innerHTML += `<br>${userInput.value}`;
+  });
+  
 });
